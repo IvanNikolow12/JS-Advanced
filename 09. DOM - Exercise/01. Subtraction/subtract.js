162fc1dc31d40d21627fc180a37a2b44607fa7b7
Ivan <112165492+IvanNikolow12@users.noreply.github.com>
2023-01-23 10:24:52 +0200

    Delete subtract.js

--- 09. DOM - Exercise/01. Subtraction/subtract.js
+++
@@ -1,7 +0,0 @@
-function subtract() {
-    const firstNumber = document.getElementById('firstNumber').value;
-    const secondNumber = document.getElementById('secondNumber').value;
-    const result = document.getElementById('result');
-    
-    result.textContent = firstNumber - secondNumber;
-}
